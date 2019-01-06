3dfa80e3918579c4df9ba3f79a5dc8f82f0f3177
m.bitar <m.bitar@aalami.me>
2019-01-06 23:58:45 +0300

    Add mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -10,24 +10,26 @@ socket.on("disconnect", function () {
 
 socket.on("newMessage", function (message) {
   var formatedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formatedTime
+  });
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formatedTime}: ${message.text}`);
-
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
   var formatedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formatedTime
+  });
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from} ${formatedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
